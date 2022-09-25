53c7af84535294eb40df2dfecbcd56c7e6387594
hktrollgames <hktrollgames@gmail.com>
2022-09-25 15:29:25 +0900

    미사일 발사

--- Assets/AiTank.cs
+++ Assets/AiTank.cs
@@ -7,6 +7,9 @@ public class AiTank : MonoBehaviour
 {
     public float attackRange = 10f;
     private float speed = 3f;
+    public GameObject missile;
+    public Transform firePos;
+    private float afterFireDelay = 1f;
 
     IEnumerator Start()
     {
@@ -15,9 +18,7 @@ public class AiTank : MonoBehaviour
             // 우리탱크로 이동.
             while (IsAttackableRange()== false) //이동
             {
-                Vector3 dir = Tank.instance.transform.position -
-                    transform.position;
-                dir.Normalize(); //-> // 크기를 1로 만듬
+                Vector3 dir = GetTargetDir(); // 크기를 1로 만듬(방향만 사용)
                 transform.Translate(dir * speed * Time.deltaTime, Space.World);
                 transform.forward = dir;
                 yield return null;
@@ -25,7 +26,21 @@ public class AiTank : MonoBehaviour
 
             // 우리탱크로 이동 했으니 미사일을 발사
             yield return null;
+
+            transform.forward = GetTargetDir();
+            var newMissile = Instantiate(missile);
+            newMissile.transform.position = firePos.position;
+            newMissile.transform.forward = transform.forward;
+            yield return new WaitForSeconds(afterFireDelay);
         }
+    }
+
+    private Vector3 GetTargetDir()
+    {
+        Vector3 dir = Tank.instance.transform.position -
+                            transform.position;
+        dir.Normalize();
+        return dir;
     }
 
     private bool IsAttackableRange()
