5e4159c490ec8b9362f70afea1864b931b233406
hktrollgames <hktrollgames@gmail.com>
2022-09-25 14:49:00 +0900

    피격 이펙트 표시

---
+++ Assets/Shell.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shell : MonoBehaviour
+{
+    public ParticleSystem explosion;
+    private void OnTriggerEnter(Collider other)
+    {
+        print(other);
+        Destroy(gameObject);
+
+        explosion.transform.parent = null;
+        explosion.Play();
+    }
+}
