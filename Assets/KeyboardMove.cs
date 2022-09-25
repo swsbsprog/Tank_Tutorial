b6174f0838ea0f711c4a6c702c80d55276359098
hktrollgames <hktrollgames@gmail.com>
2022-09-25 13:37:29 +0900

    탱크와 카메라 움직임 키본

---
+++ Assets/KeyboardMove.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardMove : MonoBehaviour
+{
+    private float speed = 5f;
+
+    void Update()
+    {
+        Vector3 move = new Vector3(0, 0, 0);
+        if (Input.GetKey(KeyCode.W)) move.z = 1;
+        if (Input.GetKey(KeyCode.A)) move.x = -1;
+        if (Input.GetKey(KeyCode.S)) move.z = -1;
+        if (Input.GetKey(KeyCode.D)) move.x = 1;
+        transform.Translate(speed * Time.deltaTime * move);
+    }
+}
---
+++ Assets/MoveToTarget.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToTarget : MonoBehaviour
+{
+    public Transform target;
+    private Vector3 offset;
+
+    private void Start()
+    {
+        offset = transform.position - target.position;
+    }
+    void LateUpdate()
+    {
+        transform.position = target.position + offset;
+    }
+}
