40cf31b6e1b6fbda5fe440ed652235394825f7cd
hktrollgames <hktrollgames@gmail.com>
2022-09-25 14:10:33 +0900

    커서, 룻엣 타겟

---
+++ Assets/Cursor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursor : MonoBehaviour
+{
+    public Vector3 mousePosition;
+    void Update()
+    {
+        mousePosition = Input.mousePosition;
+        var ray = Camera.main.ScreenPointToRay(mousePosition);
+        Physics.Raycast(ray, out var hitInfo);
+        transform.position =hitInfo.point;
+    }
+}
---
+++ Assets/LookAtTarget.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtTarget : MonoBehaviour
+{
+    public Transform target;
+
+    void Update()
+    {
+        var lookAtPos = target.position;
+        lookAtPos.y = transform.position.y;
+        transform.LookAt(lookAtPos);
+    }
+}
