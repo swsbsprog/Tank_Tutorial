bbbf47fb50abe8e0f15928b6334b869ebf890c24
hktrollgames <hktrollgames@gmail.com>
2022-09-25 17:39:45 +0900

    프로젝타일 프로젝트 추가

---
+++ Assets/ProjectileShooting/Scripts/Cursor2.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Cursor2 : MonoBehaviour 
+{    
+	void Update () 
+	{
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, float.MaxValue, 1 << LayerMask.NameToLayer("Ground")))
+        {
+            transform.position = hit.point;
+        }
+	}
+}
